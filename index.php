107e74696d02320477fdb2b8e0fa71dfa43245f8
ismail Shuaau <ismailshuaau@gmail.com>
2018-05-17 19:41:08 +0500

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+
+    $user = 'root';
+    $password = 'root';
+    $db = 'social-network';
+    $host = 'localhost';
+    $port = 3306;
+
+    
+    // PDO + Mysql
+    $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db  , $user, $password );
+
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Prepare SQL and bind parameters
+    $stmt = $pdo->prepare("INSERT INTO test (id, user_id) VALUES (:id, :user_id)");
+    $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':user_id', $user_id);
+
+    // Insert a row
+    $id = 2;
+    $user_id = 5;
+    $stmt->execute();
+
+  
+ ?>
